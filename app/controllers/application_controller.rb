e2b95e681bf0cf9e7283d18f3b21001e54d7bac3
Rana Amir <ramir2426@gmail.com>
2018-02-15 00:51:02 +0500

    notifier added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   layout :layout_by_resource
+  before_filter :prepare_exception_notifier
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActionController::RoutingError, :with => :controller_error  
   rescue_from ActiveRecord::RecordNotFound, :with => :active_record_error
@@ -20,6 +21,12 @@ class ApplicationController < ActionController::Base
     else
       "application"
     end
+  end
+  private
+  def prepare_exception_notifier
+    request.env["exception_notifier.exception_data"] = {
+      :current_user => current_user
+    }
   end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+  :email => {
+    :deliver_with => :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
+    :email_prefix => "[PREFIX] ",
+    :sender_address => %{"notifier" <notifier@partnerapp.com>},
+    :exception_recipients => %w{ramir2426@gmail.com}
+  }
 end
