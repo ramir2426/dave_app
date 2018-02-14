ae554113ef020d42728f9a7fec85c8813ca299b1
Rana Amir <ramir2426@gmail.com>
2018-02-14 07:00:49 +0500

    admin completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,29 @@ class UsersController < ApplicationController
 		redirect_to super_admin_users_path
 	end
 
+	def edit
+		@user = User.find_by_id(params[:id])
+	end
+
+	def update
+		@user = User.find_by_id(params[:id])
+		if params[:user][:password].blank?
+			@user.update_without_password(user_params)
+		else
+			@user.update_attributes(user_params)
+		end
+		respond_to do |format|
+			if @user.errors.blank?
+				format.js
+				format.html { redirect_to user_path(id: @user.id), notice: "User updated successfully." }
+			else
+				format.js
+				format.html { render :edit }
+			end
+		end
+		
+	end
+
 	private
 	def user_params
 		params.require(:user).permit(:role_id, :current_password, :first_name, :last_name, :email, :password, :password_confirmation, :percentage)
