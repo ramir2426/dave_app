a5b90ef708d494df464052f3e2a3d31b7e2f75e3
Rana Amir <ramir2426@gmail.com>
2018-02-18 02:47:57 +0500

    dynamic percentage

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,10 @@ module UsersHelper
 	def find_remaining_partner_ship(user_id)
 		parent = User.find_by_id(user_id)
 		if parent.present? && parent.descendants.present?
-			100 - (parent.descendants.sum(:percentage))
+			percent = 100 - (parent.descendants.sum(:percentage))
 		else
-			100
+			percent= 100
 		end
+		percent
 	end
 end
