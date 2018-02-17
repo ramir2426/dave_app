83078d0e5adb72ca46c8928c15b56046aae8d78d
Rana Amir <ramir2426@gmail.com>
2018-02-18 02:36:31 +0500

    company name added

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+	def find_remaining_partner_ship(user_id)
+		parent = User.find_by_id(user_id)
+		if parent.present? && parent.descendants.present?
+			100 - (parent.descendants.sum(:percentage))
+		else
+			100
+		end
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214175823) do
+ActiveRecord::Schema.define(version: 20180217212600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20180214175823) do
     t.float    "amount",                 default: 0.0
     t.float    "total_profit",           default: 0.0
     t.float    "total_loss",             default: 0.0
+    t.string   "company_name"
   end
 
   add_index "users", ["ancestry"], name: "index_users_on_ancestry", using: :btree
