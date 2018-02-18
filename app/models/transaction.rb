7bc22044c0017b9d1450ed03fcbb0caf1c26478b
Rana Amir <ramir2426@gmail.com>
2018-02-18 06:17:43 +0500

    add amount distribution

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -9,38 +9,59 @@ class Transaction < ActiveRecord::Base
   	transaction = self
   	@company_user = transaction.user
   	@total_amount =  transaction.amount
-  	if transaction.trans_types == 0
-  		if @company_user.percentage > 0
-  			@admin = User.where(role_id: 2).first
-  			amount = @total_amount*(user.percentage/100)
-			@total_amount = @total_amount - amount
-			@admin.trans_logs.create(amount: amount, pre_balance: @admin.amount, new_balance: (@admin.amount+amount), transaction_id: transaction.id )
-  			@admin.update_attributes(amount: (@admin.amount+amount))
-  		end
-		@users = @company_user.descendants
-		@users.each do |user|
-			amount = @total_amount*(user.percentage/100)
-			@total_amount = @total_amount - amount
-			parent_user = user.parent
-			parent_user.trans_logs.create(amount: amount, pre_balance: parent_user.amount, new_balance: (parent_user.amount+amount), transaction_id: transaction.id )
-			parent_user.update_attributes(amount: (parent_user.amount+amount))
-		end
-  	else
-  		if @company_user.percentage > 0
-  			@admin = User.where(role_id: 2).first
-  			amount = @total_amount*(user.percentage/100)
-			@total_amount = @total_amount - amount
-			@admin.trans_logs.create(amount: amount, pre_balance: @admin.amount, new_balance: (@admin.amount-amount), transaction_id: transaction.id )
-  			@admin.update_attributes(amount: (@admin.amount-amount))
-  		end
-		@users = @company_user.descendants
-		@users.each do |user|
-			amount = @total_amount*(user.percentage/100)
-			@total_amount = @total_amount - amount
-			parent_user = user.parent
-			parent_user.trans_logs.create(amount: amount, pre_balance: parent_user.amount, new_balance: (parent_user.amount-amount), transaction_id: transaction.id )
-			parent_user.update_attributes(amount: (parent_user.amount-amount))
-		end
-  	end
+    byebug
+  	if transaction.trans_type == 0
+      if @company_user.admin_percent > 0
+        @admin = User.where(role_id: 2).first
+        amount = @total_amount*(@company_user.admin_percent/100)
+        @total_amount = @total_amount - amount
+        @admin.trans_logs.create(amount: amount, pre_balance: @admin.amount, new_balance: (@admin.amount+amount), transaction_id: transaction.id )
+        @admin.update_attributes(amount: (@admin.amount+amount))
+      end
+      amount = @total_amount*(@company_user.percentage/100)
+      @company_user.trans_logs.create(amount: amount, pre_balance: @company_user.amount, new_balance: (@company_user.amount+amount), transaction_id: transaction.id )
+      @company_user.update_attributes(amount: (@company_user.amount+amount))
+      @company_user.children.each do |user|
+        amount = @total_amount*(user.percentage/100)
+        user.trans_logs.create(amount: amount, pre_balance: user.amount, new_balance: (user.amount+amount), transaction_id: transaction.id )
+        user.update_attributes(amount: (@admin.amount+amount))
+        nested_user_childern_profit(user, amount) if user.children.present?
+      end
+    else
+      if @company_user.admin_percent > 0
+        @admin = User.where(role_id: 2).first
+        amount = @total_amount*(@company_user.admin_percent/100)
+        @total_amount = @total_amount - amount
+        @admin.trans_logs.create(amount: amount, pre_balance: @admin.amount, new_balance: (@admin.amount-amount), transaction_id: transaction.id )
+        @admin.update_attributes(amount: (@admin.amount-amount))
+      end
+      amount = @total_amount*(@company_user.percentage/100)
+      @company_user.trans_logs.create(amount: amount, pre_balance: @company_user.amount, new_balance: (@company_user.amount-amount), transaction_id: transaction.id )
+      @company_user.update_attributes(amount: (@company_user.amount-amount))
+      @company_user.children.each do |user|
+        amount = @total_amount*(user.percentage/100)
+        user.trans_logs.create(amount: amount, pre_balance: user.amount, new_balance: (user.amount-amount), transaction_id: transaction.id )
+        user.update_attributes(amount: (@admin.amount-amount))
+        nested_user_childern_loss(user, amount) if user.children.present?
+      end
+    end
+  end
+
+  def nested_user_childern_profit(parent_user, parent_amount)
+    parent_user.children.each do |user|
+      amount =  parent_amount*(user.percentage/100)
+      user.trans_logs.create(amount: amount, pre_balance: user.amount, new_balance: (user.amount+amount), transaction_id: transaction.id )
+      user.update_attributes(amount: (user.amount+amount))
+      nested_user_childern_profit(user, amount) if user.children.present?
+    end
+  end
+
+  def nested_user_childern_loss
+    parent_user.children.each do |user|
+      amount =  parent_amount*(user.percentage/100)
+      user.trans_logs.create(amount: amount, pre_balance: user.amount, new_balance: (user.amount-amount), transaction_id: transaction.id )
+      user.update_attributes(amount: (user.amount-amount))
+      nested_user_childern_profit(user, amount) if user.children.present?
+    end
   end
 end
