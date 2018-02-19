7e83bea9e197a945318a1ccf8f108d5471ae7477
Rana Amir <ramir2426@gmail.com>
2018-02-19 14:58:57 +0500

    add adjustment funtionailty

--- app/controllers/trans_logs_controller.rb
+++ app/controllers/trans_logs_controller.rb
@@ -14,8 +14,8 @@ class TransLogsController < ApplicationController
 
   # GET /trans_logs/new
   def new
-    user = User.find_by_id(params[:user_id])
-    @trans_log = user.trans_logs.new
+    @user = User.find_by_id(params[:user_id])
+    @trans_log = @user.trans_logs.new
   end
 
   # GET /trans_logs/1/edit
@@ -25,11 +25,12 @@ class TransLogsController < ApplicationController
   # POST /trans_logs
   # POST /trans_logs.json
   def create
+    @user = User.find_by_id(params[:trans_log][:user_id])
     @trans_log = TransLog.new(trans_log_params)
-
+    update_user_account
     respond_to do |format|
       if @trans_log.save
-        format.html { redirect_to @trans_log, notice: 'Trans log was successfully created.' }
+        format.html { redirect_to adjustment_transactions_path, notice: 'Adjustment was successfully created.' }
         format.json { render :show, status: :created, location: @trans_log }
       else
         format.html { render :new }
@@ -71,5 +72,13 @@ class TransLogsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def trans_log_params
       params.require(:trans_log).permit(:amount, :pre_balance, :new_balance, :transaction_id, :user_id, :trans_type, :comment)
+    end
+
+    def update_user_account
+      amount = 0
+      amount = @trans_log.trans_type == 0 ? @user.amount+@trans_log.amount : @user.amount-@trans_log.amount
+      @trans_log.pre_balance =  @user.amount
+      @trans_log.new_balance = amount
+      @user.update_attributes(amount: amount)
     end
 end
