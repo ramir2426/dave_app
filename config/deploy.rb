890714e2fe9350619a2ef28eac0f3a1b0dc54f35
Rana Amir <ramir2426@gmail.com>
2018-02-20 07:22:10 +0500

    delete gem lock gile

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ before "deploy:assets:precompile", "bundle:install"
 append :linked_files, "config/database.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 server '54.37.16.17', user: 'deploy', roles: %w{app db web}
+set :bundle_without, [:development, :test]
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
