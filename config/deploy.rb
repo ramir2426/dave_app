e19c1fd204ee37ab56d046b0443bf9bef1747ad4
Rana Amir <ramir2426@gmail.com>
2018-02-20 16:12:30 +0500

    release 0nly 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ server '54.37.16.17', user: 'deploy', roles: %w{app db web}
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-set :keep_releases, 5
+set :keep_releases, 3
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
